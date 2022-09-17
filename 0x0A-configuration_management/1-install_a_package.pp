6849f0a6d3a16d3191097981fafcc9bf6c32c0b9
LIFE-arch <agbajetayo63gbajetayo63@gmail.com>
2022-09-16 17:30:54 -0700

    con

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/env pup
-# This manifests 
+# This manifests
 package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
-  source   => 'https://rubygems.org',
+ensure   => '2.5.0',
+provider => 'gem',
+source   => 'https://rubygems.org',
 }
